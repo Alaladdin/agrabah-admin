732522c3186000ea144f2080bb02bbee2e02d565
Aladdin <g.tabroid@gmail.com>
2021-09-30 00:25:51 +0300

    v 0.1.31

--- pages/bots/vk.vue
+++ pages/bots/vk.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="">
-    <textarea v-model.trim="message" class="textarea mb-5 h-70 resize-none" />
+  <div>
+    <div>
+      <textarea v-model.trim="message" class="textarea mb-5 h-70 resize-none" />
 
-    <div class="flex justify-end items-center">
-      <Select v-model="vkSendToChatId" :options="chatSelectOptions" class="mr-5" />
-      <Button text="Send" :disabled="isSendDisabled" :loading="isSending" @click="sendMess" />
+      <div class="flex justify-end items-center">
+        <Select v-model="vkSendToChatId" :options="chatSelectOptions" class="mr-5" />
+        <Button text="Send" :disabled="isSendDisabled" :loading="isSending" @click="sendMess" />
+      </div>
+    </div>
+
+    <div>
+      <h3 class="my-6 text-xl font-semibold text-center">
+        Bot Config
+      </h3>
+
+      <div class="flex justify-center">
+        <div class="bg-white p-3 rounded w-max">
+          <div
+            v-for="({ title,value }, index) in botConfigFields"
+            :key="index"
+            class="flex justify-between p-1 w-100"
+          >
+            {{ title }} <span>{{ value }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +43,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters('vk', { vkChats: 'getVKChats' }),
+    ...mapGetters('vk', { vkChats: 'getVKChats', botConfig: 'getBotConfig' }),
 
     chatSelectOptions () {
       return map(this.vkChats, chat => ({
@@ -31,15 +51,32 @@ export default {
         value: chat.chatId,
       }))
     },
+    botConfigFields () {
+      if (!this.botConfig) return []
+
+      const { isEnabled: isAutopostingEnabled } = this.botConfig.actualityAutoposting
+
+      return [
+        {
+          title: 'Состояние бота',
+          value: this.botConfig ? 'включен' : 'выключен',
+        },
+        {
+          title: 'Автопостинг актуалочки',
+          value: isAutopostingEnabled ? 'включен' : 'выключен',
+        },
+      ]
+    },
     isSendDisabled () {
       return this.message.length < 5 || this.isSending
     },
   },
   mounted () {
     this.vkSendToChatId = this.vkChats[0].chatId
+    this.getBotConfig()
   },
   methods: {
-    ...mapActions('vk', ['sendMessage']),
+    ...mapActions('vk', ['sendMessage', 'getBotConfig']),
 
     sendMess () {
       this.isSending = true
--- store/vk.js
+++ store/vk.js
@@ -2,14 +2,33 @@ import vkChats from '@/data/chats'
 
 export const state = () => ({
   vkChats,
+  botConfig: null,
 })
 
 export const getters = {
-  getVKChats: state => state.vkChats,
+  getVKChats  : state => state.vkChats,
+  getBotConfig: state => state.botConfig,
+}
+
+export const mutations = {
+  setBotConfig (state, botConfig) {
+    state.botConfig = botConfig
+  },
 }
 
 export const actions = {
   sendMessage (ctx, { message, chatId = vkChats[0].chatId, parseLinks = false }) {
     return this.$api.$post('/vk/sendMessage', { message, chatId, parseLinks })
+  },
+  getBotConfig (ctx) {
+    return this.$api.$get('/vk/getStore')
+      .then((data) => {
+        ctx.commit('setBotConfig', data.store)
+
+        return data.store
+      })
+      .catch((err) => {
+        throw err
+      })
   },
 }
