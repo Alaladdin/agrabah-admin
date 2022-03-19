6936491181e21f425b74a35787d51bc7045bd53c
Aladdin <g.tabroid@gmail.com>
2022-03-19 19:31:15 +0300

    v 1.14.10

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,14 +1,14 @@
 export const validateUsername = (username) => {
   const usernameLength = username.trim().length
 
-  return usernameLength >= 4 && usernameLength <= 15
+  return usernameLength >= 3 && usernameLength <= 15
 }
 
 export const validateDisplayName = (displayName) => {
   if (displayName) {
     const displayNameLength = displayName.trim().length
 
-    return displayNameLength >= 4 && displayNameLength <= 15
+    return displayNameLength && displayNameLength <= 15
   }
 
   return true
