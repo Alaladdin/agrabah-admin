ec8b026ec9dfbed25adf9d001f9874492c15aec4
Aladdin <g.tabroid@gmail.com>
2022-03-26 00:33:59 +0300

    v 1.15.16

--- components/b-progress-bar.vue
+++ components/b-progress-bar.vue
@@ -30,10 +30,10 @@ export default {
       const prefix = 'progress__bar--'
 
       if (this.value <= 40)
-        return prefix + (this.reverseColors ? 'low' : 'high')
+        return prefix + (this.reverseColors ? 'high' : 'low')
 
       if (this.value >= 70)
-        return prefix + (this.reverseColors ? 'high' : 'low')
+        return prefix + (this.reverseColors ? 'low' : 'high')
 
       return prefix + 'medium'
     },
