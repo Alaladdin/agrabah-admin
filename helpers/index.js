621939871ca417f91f1d70decf4bd3f5fd77fa83
Aladdin <g.tabroid@gmail.com>
2022-05-31 02:05:20 +0300

    v 1.21.2

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,4 @@
-import { cloneDeep, map, isArray, isObject, isString, random, isUndefined } from 'lodash'
+import { cloneDeep, map, isArray, isObject, isString, random, isNull } from 'lodash'
 import { nanoid } from 'nanoid'
 import moment from 'moment'
 import { validateUsername, validateDisplayName, validatePassword, validateUrl } from './validators'
@@ -40,7 +40,7 @@ const getFromLocalStorage = (key, defaultValue = {}) => {
 
   val = val && isJson(val) && JSON.parse(val)
 
-  return isUndefined(val) ? defaultValue : val
+  return isNull(val) ? defaultValue : val
 }
 
 const formatDate = (date, format = 'DD.MM.YYYY') => moment(date).format(format)
