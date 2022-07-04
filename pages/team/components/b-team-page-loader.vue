b4535b73a4185172b41e18a9169ef05e7b15b773
Aladdin <g.tabroid@gmail.com>
2022-07-04 03:26:40 +0300

    v 1.21.4

--- pages/team/components/b-team-page-loader.vue
+++ pages/team/components/b-team-page-loader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="relative">
     <div class="absolute w-full h-full z-1 bg-gradient-to-b from-transparent to-gray-200" />
+    <div class="py-3.5 ml-auto mb-4 rounded w-20 bg-white animate-pulse" />
+
     <div class="grid grid-cols-2 gap-4">
       <div v-for="userNum in 12" :key="userNum" class="updown__item !py-9 animate-pulse" :class="{ '!shadow-none' : userNum >= 10 }" />
     </div>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -2,13 +2,16 @@
   <div class="team">
     <b-team-page-loader v-if="isLoading" />
 
-    <div v-if="!isLoading && data" class="grid grid-cols-2 gap-4">
-      <b-team-item
-        v-for="user in data"
-        :key="user._id"
-        :user="user"
-      />
-    </div>
+    <template v-if="!isLoading && data">
+      <b-button class="flex self-end mb-5" text="Refresh" variant="white" @click="refresh" />
+      <div class="grid grid-cols-2 gap-4">
+        <b-team-item
+          v-for="user in data"
+          :key="user._id"
+          :user="user"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -16,13 +19,18 @@
 import BTeamPageLoader from './components/b-team-page-loader'
 import PageDefaultMixin from '@/mixins/m-page-default'
 import BTeamItem from '@/pages/team/components/b-team-item'
+import BButton from '@/components/b-button'
 
 export default {
   name      : 'team',
   components: {
+    'b-button'          : BButton,
     'b-team-item'       : BTeamItem,
     'b-team-page-loader': BTeamPageLoader,
   },
   mixins: [PageDefaultMixin('team')],
+  data  : () => ({
+    clearDataOnDestroy: false,
+  }),
 }
 </script>
