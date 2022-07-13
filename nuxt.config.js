1d9f33e1e86e032c9979b8aa404c1f9b1fbb909c
Aladdin <g.tabroid@gmail.com>
2022-07-14 02:43:33 +0300

    v 1.22.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,16 @@ export default {
     meta : [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'AGRABAH ADMIN SIDE' },
+      { hid: 'description', name: 'description', content: 'AGRABAH ADMIN' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   css   : ['@/assets/scss/main.scss'],
   render: {
     asyncScripts: true,
+  },
+  compressor: {
+    level: 9,
   },
   plugins: [
     '~/plugins/axios',
