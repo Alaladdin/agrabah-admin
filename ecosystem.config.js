1b003b185536ff0b37e4759c20ffb8b7ea4b0daa
Aladdin <g.tabroid@gmail.com>
2022-07-14 00:57:26 +0300

    GitHub actions

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,11 @@ module.exports = {
       exec_mode     : 'cluster',
       script        : './node_modules/nuxt/bin/nuxt.js',
       args          : 'start',
-      watch         : true,
-      ignore_watch  : ['node_modules'],
       env           : { NODE_ENV: 'development' },
       env_production: { NODE_ENV: 'production' },
     },
   ],
   deploy: {
-    // "production" is the environment name
     production: {
       key          : process.env.DEPLOY_KEY,
       user         : process.env.DEPLOY_USER,
@@ -23,7 +20,11 @@ module.exports = {
       repo         : 'https://github.com/Alaladdin/agrabah-admin',
       path         : process.env.DEPLOY_PATH,
       'pre-deploy' : 'pm2 stop ./ecosystem.config.js',
-      'post-deploy': 'npm install; npm run build && pm2 start ./ecosystem.config.js --env production --update-env',
+      'post-deploy': [
+        'npm install --production',
+        'npm run build',
+        'pm2 start ./ecosystem.config.js --env production --update-env',
+      ].join('; '),
     },
   },
 }
